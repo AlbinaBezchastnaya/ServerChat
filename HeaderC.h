b111bfdac949c8a9543b928e1ab662f94d5ba383
AlbinaBezchastnaya <Albina.Bezchastnaya@yandex.ru>
2024-11-11 15:57:59 +1000

    Add files via upload

---
+++ HeaderC.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include <map>
+#include <vector>
+
+void userReg(std::string& curName, std::string& curPassWord);  // enter user's data while registеring or loggin in
+
+class Chat
+{
+    std::map <std::string, std::string> persArray;               // array of user's data
+public:
+    void entrChat(bool& persPresence, std::string& curName, std::string& curPassWord);     // to log in chat
+    void regChat(bool& nameInChatAlready, bool& persPresence, std::string& curName, std::string& curPassWord);    // to register in chat
+    void sendMess(std::string curName);            // to send a message
+};
